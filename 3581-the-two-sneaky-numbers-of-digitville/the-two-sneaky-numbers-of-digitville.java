bb99e52fd50345086cb19996e3fe80f1a52ad707
Lijashree_B <lijashree340@gmail.com>
2026-06-25 15:00:48 +0530

    Time: 1 ms (99.87%) | Memory: 46.1 MB (51.07%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
         int[] freq=new int[101];
+
         int[] ans=new int[2];
         int index=0;
-        //for()
         for(int num:nums){
             freq[num]++;
             if(freq[num]==2){
