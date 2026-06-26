9191393d952a5ec77030eaee9f4ebd09cf05135b
Lijashree_B <lijashree340@gmail.com>
2026-06-26 09:47:18 +0530

    Time: 2 ms (44.78%) | Memory: 43.5 MB (97.74%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> path=new Stack<>();
+        for(String log:logs){
+            if(log.equals("../")){
+                if (!path.isEmpty()){
+                    path.pop();
+                }
+            } 
+            else if(!log.equals("./")){
+                path.push(log);
+            }
+        }
+        return path.size();
+    }
+}
