d6b5d8131f394df75d4aac7b44d6be8d17dc9f03
Lijashree_B <lijashree340@gmail.com>
2026-07-14 11:38:22 +0530

    Time: 3 ms (41.93%) | Memory: 46.6 MB (89.23%) - LeetSync

---
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            list.add(arr[i]);
+            if(arr[i]==0){
+                list.add(0);
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            arr[i]=list.get(i);
+        }
+        //list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
