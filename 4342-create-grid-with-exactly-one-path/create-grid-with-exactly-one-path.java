7309f5adba4b2aad5219d8b2f5a790bd7fcbba66
Lijashree_B <lijashree340@gmail.com>
2026-07-28 10:04:25 +0530

    Time: 2 ms (94.98%) | Memory: 46.2 MB (99.06%) - LeetSync

---
+++ 4342-create-grid-with-exactly-one-path/create-grid-with-exactly-one-path.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String[] createGrid(int m, int n) {
+        char[][] grid = new char[m][n];
+        for(int i=0;i<m;i++){
+            Arrays.fill(grid[i],'#');
+        }
+        Arrays.fill(grid[0],'.');
+        for (int i=0;i<m;i++){
+            grid[i][n-1]='.';
+        }
+        String[] ans=new String[m];
+        for (int i=0;i<m;i++) {
+            ans[i]=new String(grid[i]);
+        }
+        return ans;
+    }
+}
